7042c1f1395546cc4955117c95dc9eea5124d904
Lidia Palacios Toledo <lpalacio@c3r13s3.42madrid.com>
2024-01-02 21:07:30 +0100

    starting searching for duplicates and first sort

---
+++ sort_and_duplicates.c
@@ -0,0 +1,27 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   sort_and_duplicates.c                              :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: lpalacio <lpalacio@student.42madrid>       +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2024/01/02 20:59:36 by lpalacio          #+#    #+#             */
+/*   Updated: 2024/01/02 21:06:36 by lpalacio         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+void	fill_array(t_list	stack)
+{
+	int	*num;
+
+	num = (int *) ft_calloc(ft_lstsize(stack), sizeof(int));
+	
+	
+	return ;
+}
+
+void	sort(t_list stack)
+{
+	return ;
+}
+
