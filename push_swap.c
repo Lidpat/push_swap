d7b1a8c36f0c9be214a08b54a01182ce0812cbe8
LidPat <lpalacio@student.42madrid.com>
2024-03-13 23:12:08 +0100

    Eppur si muove: move elements as per condition

--- push_swap.c
+++ push_swap.c
@@ -38,22 +38,56 @@ int	get_bits_number(int	lst_size)
 }
 
 //function applicable to ft_lstiter f(aux->content)
-void	move_zero_right(t_list *lst, int bit)
+void	move_zero_right(t_list **lst_src, t_list **lst_dst, int bit)
 {
 	int		numb;
 	int		bit_value;
 	t_list	*aux;
+	int stack_size;
 
-	aux = lst;
-	while (aux != NULL)
+	aux = *lst_src;
+	stack_size = ft_lstsize(aux);
+	while (stack_size--)
 	{
 		numb = ((int *)(aux->content))[position];
 		bit_value = (numb >> bit) % 2;
-		ft_putnbr_fd (numb, 1);
-		write (1, "\t", 1);
-		ft_putnbr_fd (bit_value, 1);
-		write (1, "\n", 1);
-		aux = aux->next; 
+		if (bit_value == 0)
+			push(lst_src, lst_dst);
+		else
+			rotate(lst_src);
+//		ft_putnbr_fd (numb, 1);
+//		write (1, "\t", 1);
+//		ft_putnbr_fd (bit_value, 1);
+//		write (1, "\n", 1);
+		//aux = aux->next;
+		aux = *lst_src; 
+	}
+	
+}
+
+void	move_one_left(t_list **lst_src, t_list **lst_dst, int bit)
+{
+	int		numb;
+	int		bit_value;
+	t_list	*aux;
+	int stack_size;
+
+	aux = *lst_src;
+	stack_size = ft_lstsize(aux);
+	while (stack_size--)
+	{
+		numb = ((int *)(aux->content))[position];
+		bit_value = (numb >> bit) % 2;
+		if (bit_value == 1)
+			push(lst_src, lst_dst);
+		else
+			rotate(lst_src);
+//		ft_putnbr_fd (numb, 1);
+//		write (1, "\t", 1);
+//		ft_putnbr_fd (bit_value, 1);
+//		write (1, "\n", 1);
+		//aux = aux->next;
+		aux = *lst_src; 
 	}
 	
 }
@@ -72,32 +106,37 @@ int	push_swap (t_list *stack)
 	stack_a = stack;
 	stack_b = NULL;
 	bit = 0;
-	
 	stack_size = ft_lstsize(stack);
 	bit_max = get_bits_number(stack_size);
 	//while (bit < bit_max)
+		ft_lstiter(stack_a, ft_putnbr_endl);
+	write (1, "\n", 1);
 	while(bit < bit_max)
 	{
-	//	stack_a = stack;
-	//	stack_size = ft_lstsize(stack_a);
-		move_zero_right(stack_a, bit);
-/*		while (stack_size --)
-		{
-			numb = ((int *)(stack_a->content))[position];
-			bit_value = (numb >> bit) % 2;
-			ft_putnbr_fd (numb, 1);
-			write (1, "\t", 1);
-			ft_putnbr_fd (bit_value, 1);
-			write (1, "\n", 1);
-			stack_a = stack_a->next;
-		}
-*/		write (1, "---\n", 4);
+		move_zero_right(&stack_a, &stack_b, bit);
+		write (1, "---\n", 4);
 		bit++;
 	}
 	
+	bit = bit_max - 1;
+	while(bit >= 0)
+	{
+		move_one_left(&stack_b, &stack_a, bit);
+		write (1, "---\n", 4);
+		bit--;
+	}
+	push(&stack_b, &stack_a);
+
+		write (1, "* Stack_A\n", 10);
+	ft_lstiter(stack_a, ft_putnbr_endl);
+	write (1, "\n", 1);
+	write (1, "* Stack_B\n", 10);
+	ft_lstiter(stack_b, ft_putnbr_endl);
+
 	//VARS
 	//  *int instruction_list (int[]) //integer string mapear valores movimientos (1=sa 2=sa 3=ss 4=ra 5=rb...  10=rrb 11=rrr) 
 	return (0);
+	
 }
 
 #include <stdio.h> // BORRAR
@@ -158,8 +197,8 @@ int main (int argc, char *argv[])
 	ft_lstclear(&stack_a, free);
 	ft_lstclear(&stack_b, free);
 */	
-	write (1, "- - -- - -\n", 11);
-	ft_lstiter(stack, ft_putnbr_endl);  //print stack_a
+//	write (1, "- - -- - -\n", 11);
+//	ft_lstiter(stack, ft_putnbr_endl);  //print stack_a
 	
 /************ Fin Test Block *******************************************************************/
 
